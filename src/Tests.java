a88a86f08d345ca878f5eb17df3d6acd4c4dc9ef
Electrocaruzo <gongrossman@gmail.com>
2025-04-23 12:32:59 +0300

    made the tests work

--- src/Tests.java
+++ src/Tests.java
@@ -1,13 +1,29 @@
-import java.util.HashMap;
-
 public class Tests {
-    static HashMap<Solution, String> solutions = new HashMap<Solution, String>();
+    public static final String ANSI_RESET = "\u001B[0m";
+    public static final String ANSI_RED = "\u001B[31m";
+    public static final String ANSI_GREEN = "\033[0;92m";
 
-    public static void fillHashMap() {
-        solutions.put(new P001(), "233168");
+    static String[][] solutions = new String[][]{
+            {new P001().run(), "233168"}
+    };
+
+    public static String getNameOfProblemFromIndex(int index) {
+        StringBuilder builder = new StringBuilder("P");
+        index++;
+        if (index < 100) {
+            builder.append(0);
+        }
+        if (index < 10) {
+            builder.append(0);
+        }
+        builder.append(index);
+        return builder.toString();
     }
 
     public static void main(String[] args) {
-
+        for (int i = 0; i < solutions.length; i++) {
+            boolean success = (solutions[i][0].equals(solutions[i][1]));
+            System.out.println(getNameOfProblemFromIndex(i) + " success: " + (success ? ANSI_GREEN : ANSI_RED) + success + ANSI_RESET);
+        }
     }
 }
